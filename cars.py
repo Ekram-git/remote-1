de84ce827a09900d00a9f9a190ea20c1dba1e75c
Ekram <Ekramkarim@hotmail.com>
2024-01-31 12:22:02 +0300

    Adding random year modedl function

--- cars.py
+++ cars.py
@@ -5,8 +5,12 @@ def get_random_car_brand():
     random_select = random.choice(car_brands)
     return random_select
 
+def get_random_model_year():
+    return random.randint(2000, 2022)
 
 if __name__ == "__main__":
     random_car = get_random_car_brand()
-    print("Randomly selected car brand:", random_car)
+    random_year = get_random_model_year()
 
+    print("Randomly selected car brand:", random_car)
+    print("Random model year:", random_year)
