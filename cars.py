e5dfd5143605bcfd3ef2f308eb62a5dcf1023198
Ekram <Ekramkarim@hotmail.com>
2024-01-31 12:11:21 +0300

    adding random to python

--- cars.py
+++ cars.py
@@ -1,5 +1,12 @@
-def cars():
-    print('cars')
+import random
 
-if __name__ == '__main__':
-    cars()
+def get_random_car_brand():
+    car_brands = ["Toyota", "Honda", "Ford", "BMW", "Volkswagen", "BMW"]
+    random_select = random.choice(car_brands)
+    return random_select
+
+
+if __name__ == "__main__":
+    random_car = get_random_car_brand()
+    print("Randomly selected car brand:", random_car)
+
